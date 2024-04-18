575a711c386b80548e9f1bfb0eb3d33363c0103d
nizhdanov <zhdanov.delovayakolbasa@gmail.com>
2024-04-19 02:00:09 +0500

    :adhesive_bandage:

---
+++ packages/eslint/index.js
@@ -0,0 +1,5 @@
+import react from './eslint.config.react.mjs';
+
+const eslint = { react: react };
+
+export { eslint };
--- packages/prettier/.prettierrc.tw.mjs
+++ packages/prettier/.prettierrc.tw.mjs
@@ -1,8 +1,14 @@
-import base from './.prettierrc.base.mjs';
-
 /** @type {import("prettier").Config} */
 export default {
-  ...base,
+  printWidth: 100,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  arrowParens: 'always',
   tailwindFunctions: ['cn', 'cva', 'clsx'],
   plugins: ['prettier-plugin-tailwindcss']
 };
